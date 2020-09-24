bbddc7963420f2e5c395e84ad39e3e4c5132fe43
Tom <tom.rix@munol.org>
2020-09-24 20:41:00 +0200

    file naming fixed

--- tocfixes.py
+++ tocfixes.py
@@ -2,8 +2,8 @@
 Mit diesem Skript wird eine pdftk data_dump Datei bearbeitet, sodass die Bookmark Eintraege des Inhaltsverzeichnisses um zwei erhoeht werden. Dies ist notwendig, weil beim mergen des Mantelbogens mit dem Inhalt zwei zusaetzliche Seiten vorne angehaengt werden.
 '''
 
-with open('metadata.txt') as file:
-    with open('metadata_updated.txt','a') as outfile:
+with open('metadaten.txt') as file:
+    with open('metadaten_updated.txt','a') as outfile:
         line = file.readline()
         while line:
             if 'BookmarkPageNumber' in line:
