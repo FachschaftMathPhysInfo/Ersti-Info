1e14123b19ebbc63ec597d5662a667aa9dba807d
Tom Rix <tom.rix@web.de>
2021-09-03 16:34:07 +0200

    Usage und output angepasst, weitere Verbesserungen

--- bilder/generate_semesterbeitrag_piechart.py
+++ bilder/generate_semesterbeitrag_piechart.py
@@ -1,23 +1,30 @@
 '''
 Dieses Skript ermöglicht das Erstellen eines Tortendiagramms zur Visualisierung des Semesterbeitrags im XKCD Style.
 
-Usage: python generate_semesterbeitrag_piechart.py
-Output: semesterbeitrag.png
+Usage: python3 bilder/generate_semesterbeitrag_piechart.py
+Outputs: bilder/semesterbeitrag.png, bilder/semesterbeitrag_grayscale.png
 '''
 
 import matplotlib.pyplot as plt
-import PIL
+from PIL import Image
 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'VS-Beitrag', 'VRNextbike',  'Studierendenwerk', 'VRN-Semesterticket','Verwaltungskostenbeitrag'
-sizes = [10, 2.50, 54,35.30,  70]
-explode = (0.2, 0, 0, 0, 0) # moved a bit outwards for better readablility
+
+# Values taken from https://www.stura.uni-heidelberg.de/finanzen/vs-beitrag/
+sizes = [10, 2.5, 54, 35.3, 70]
+
+explode = (0.2, 0, 0, 0, 0) # moved a bit outwards for better readability
+
 colors = ['tab:orange', 'tab:blue', 'tab:green','tab:purple',  'tab:red']
 
 
 with plt.xkcd(): # enables xkcd style
+
     plt.figure(figsize=(7,4))
+
     plt.title("Wohin fließt mein Semesterbeitag?")
+
     p, tx, autotexts = plt.pie(sizes, explode=explode, labels=labels, autopct='',
             shadow=True, startangle=310, colors=colors)
     
@@ -29,8 +36,6 @@ with plt.xkcd(): # enables xkcd style
 
     plt.savefig('bilder/semesterbeitrag')
 
-    # grayscale version for printing
-    from PIL import Image
+    # grayscale version for printing    
     img = Image.open('bilder/semesterbeitrag.png').convert('LA')
     img.save('bilder/semesterbeitrag_grayscale.png')
-
