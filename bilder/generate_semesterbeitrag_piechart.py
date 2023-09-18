3a668a6c676d528dbd861081cebb43cbbb61e367
Splines <dominic-plein@gmx.de>
2023-09-18 03:35:15 +0200

    Update Semesterbeitrag pie chart

--- bilder/generate_semesterbeitrag_piechart.py
+++ bilder/generate_semesterbeitrag_piechart.py
@@ -1,6 +1,8 @@
 '''
 Dieses Skript ermöglicht das Erstellen eines Tortendiagramms zur Visualisierung des Semesterbeitrags im XKCD Style.
 
+Möglicherweise muss das Bild im Anschluss in einer Vektorgrafiksoftware nachbearbeitet werden, um die Labels besser zu positionieren.
+
 Usage: python3 bilder/generate_semesterbeitrag_piechart.py
 Outputs: bilder/semesterbeitrag.png, bilder/semesterbeitrag_grayscale.png
 '''
@@ -9,33 +11,40 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'VS-Beitrag', 'VRNextbike',  'Studierendenwerk', 'VRN-Semesterticket','Verwaltungskostenbeitrag'
+labels = 'VS-Beitrag', 'VRNextbike',  'Theaterflatrate', 'Studierendenwerk',  'Verwaltungskostenbeitrag'
 
 # Values taken from https://www.stura.uni-heidelberg.de/finanzen/vs-beitrag/
-sizes = [10, 2.5, 54, 35.3, 70]
+sizes = [10, 2.55, 2.5, 66, 70]
 
-explode = (0.2, 0, 0, 0, 0) # moved a bit outwards for better readability
+explode = (0, 0.2, 0.5, 0, 0)  # moved a bit outwards for better readability
 
-colors = ['tab:orange', 'tab:blue', 'tab:green','tab:purple',  'tab:red']
+# see https://matplotlib.org/stable/gallery/color/named_colors.html
+# for more colors
+colors = ['tab:orange', 'tab:blue', 'tab:cyan',
+          'tab:purple',  'tab:red']
 
 
-with plt.xkcd(): # enables xkcd style
+with plt.xkcd():  # enables xkcd style
 
-    plt.figure(figsize=(7,4))
+    plt.figure(figsize=(11, 4))
 
-    plt.title("Wohin fließt mein Semesterbeitag?")
+    plt.title("Wohin fließt mein Semesterbeitrag?")
 
-    p, tx, autotexts = plt.pie(sizes, explode=explode, labels=labels, autopct='',
-            shadow=True, startangle=310, colors=colors)
-    
-    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    p, tx, autotexts = plt.pie(sizes, explode=explode, labels=labels,
+                               autopct='', shadow=True, startangle=0, colors=colors)
+
+    # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.axis('equal')
 
     # sets description with actual values. No decimal places if round value, otherwise two places
     for i, a in enumerate(autotexts):
-        a.set_text("{:.2f} €".format(sizes[i]).replace('.00','').replace('.',','))
+        a.set_text("{:.2f} €"
+                   .format(sizes[i])
+                   .replace('.00', '')
+                   .replace('.', ','))
 
-    plt.savefig('bilder/semesterbeitrag')
+    plt.savefig('bilder/semesterbeitrag', dpi=300)
 
-    # grayscale version for printing    
+    # grayscale version for printing
     img = Image.open('bilder/semesterbeitrag.png').convert('LA')
     img.save('bilder/semesterbeitrag_grayscale.png')
